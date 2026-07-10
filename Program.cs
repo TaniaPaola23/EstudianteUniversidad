cfa4d5a16b45febe102d8d0ab3bc72f764aa24d3
Tania Paola Mendez Quijano <taniamendezq23@gmail.com>
2026-07-10 15:57:49 -0600

    Adapta Program.cs a la estructura clásica de C#

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EstudianteUniversidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
